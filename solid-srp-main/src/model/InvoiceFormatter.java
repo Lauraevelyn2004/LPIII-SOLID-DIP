d3af3aba195f444733559e01dd58abadfdaa7b3f
Laura Evelyn Neves de Oliveira <56083012856@CAMPUS.MACHADO.MCH>
2025-12-16 13:52:10 -0300

    repositório correto

--- solid-srp-main/src/model/InvoiceFormatter.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public interface InvoiceFormatter {
-    String formatInvoice(Invoice invoice);
-}
--- solid-srp-main/src/model/Notifier.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public interface Notifier {
-    void notify(String contact, String formattedContent);
-}
